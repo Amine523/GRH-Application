1f430609ed8a3eb97ef19ab6184c1e4442bab292
Saif Eddine Ayedi <Saif.ayedi@softtodo.com>
2024-10-03 16:12:45 +0100

    [TASK] add create User Interface .

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -2,15 +2,19 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import UpdatePasswordForm from '/resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from '/resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue';
-import {Head} from '@inertiajs/vue3';
+import {Head, router} from '@inertiajs/vue3'; // import router
 import UserList from "@/Pages/Users/Partials/UserList.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 defineProps({
-
     users: {
         type: Object,
     },
 });
+
+const addUser = () => {
+    router.get(route('users.create'));
+};
 </script>
 
 <template>
@@ -24,6 +28,11 @@ defineProps({
         <div class="py-12">
             <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
                 <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                    <div class="flex justify-end">
+                        <PrimaryButton @click="addUser" class="bg-green-600 text-white">
+                            Add New Employed
+                        </PrimaryButton>
+                    </div>
                     <UserList :users="users"></UserList>
                 </div>
             </div>
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Our Platform</title>
+</head>
+<body>
+<h1>Hello {{ $user->email }},</h1>
+<p>Welcome to our platform! We are excited to have you.</p>
+</body>
+</html>
