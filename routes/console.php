e1ea0073ff366c1357c9bcb6abddfa61d406f31c
Saif Eddine Ayedi <Saif.ayedi@softtodo.com>
2024-10-22 11:27:42 +0100

    [TASK] add cron for rest authorization hours .

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('authorization:reset')->everyMinute();
+Schedule::command('authorization:reset')->monthly();
